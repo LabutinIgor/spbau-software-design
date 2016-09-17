2f0e6ba3f4145a952a14b87e63987ba839b7ed1c
LabutinIgor <Labutin.IgorL@gmail.com>
2016-09-17 19:12:16 +0300

    Initial commit

---
+++ src/main/java/ru/spbau/mit/Main.java
@@ -0,0 +1,12 @@
+package ru.spbau.mit;
+
+import java.io.IOException;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
