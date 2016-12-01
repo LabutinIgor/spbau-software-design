e5ab32b0864c95359a77a07ffce6070c46bcaadd
LabutinIgor <Labutin.IgorL@gmail.com>
2016-12-01 20:23:38 +0300

    Made Gprc server and client

--- src/main/java/ru/spbau/mit/MessengerServer.java
+++ src/main/java/ru/spbau/mit/MessengerServer.java
@@ -1,16 +1,25 @@
 package ru.spbau.mit;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import ru.spbau.mit.proto.Message;
+import ru.spbau.mit.proto.MessengerGrpc;
+
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * The MessengerServer class provides realisation of server that can send and receive messages
  */
 public class MessengerServer {
+    private static Logger logger = Logger.getLogger(MessengerServer.class.getName());
+
     private MessagesReceiver messagesReceiver;
     private int port;
-    private Connection connection = null;
+    private Server server;
+    private StreamObserver<Message> observer;
 
     public MessengerServer(int port, MessengerGUIMain messengerGUIMain) {
         this.port = port;
@@ -23,11 +32,13 @@ public class MessengerServer {
     public synchronized void start() throws IOException {
         new Thread(() -> {
             try {
-                ServerSocket serverSocket = new ServerSocket(port);
-                Socket socket = serverSocket.accept();
-                connection = new Connection(socket.getInputStream(), socket.getOutputStream(), messagesReceiver);
-                connection.start();
-            } catch (IOException ignored) {
+                server = ServerBuilder.forPort(port)
+                        .addService(new MessengerService())
+                        .build()
+                        .start();
+                server.awaitTermination();
+            } catch (IOException | InterruptedException exception) {
+                logger.log(Level.WARNING, "Error in receiving messages: ", exception);
             }
         }).start();
     }
@@ -36,16 +47,44 @@ public class MessengerServer {
      * This method sends message to client
      */
     public synchronized void sendMessage(String name, String message) throws IOException {
-        if (connection == null) {
+        if (observer == null) {
             throw new IOException("No client connected");
         }
-        connection.sendMessage(name, message);
+        observer.onNext(Message.newBuilder().setName(name).setContent(message).build());
     }
 
     /**
      * This method stops server
      */
     public synchronized void stop() {
-        connection.stop();
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    /**
+     * The MessengerService class is service class for using gprc
+     */
+    private class MessengerService extends MessengerGrpc.MessengerImplBase {
+        @Override
+        public StreamObserver<Message> chat(final StreamObserver<Message> responseObserver) {
+            return new StreamObserver<Message>() {
+                @Override
+                public void onNext(Message message) {
+                    messagesReceiver.receiveMessage(message.getName(), message.getContent());
+                    observer = responseObserver;
+                }
+
+                @Override
+                public void onError(Throwable exception) {
+                    logger.log(Level.WARNING, "Error in grpc service: ", exception);
+                }
+
+                @Override
+                public void onCompleted() {
+                    responseObserver.onCompleted();
+                }
+            };
+        }
     }
 }
