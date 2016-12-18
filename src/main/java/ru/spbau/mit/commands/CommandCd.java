359d90d7c5ac41683055c9c6466084b13ac75d3a
N-buga <bugakova_nadya@mail.ru>
2016-12-19 00:32:20 +0300

    fixed bug

--- src/main/java/ru/spbau/mit/commands/CommandCd.java
+++ src/main/java/ru/spbau/mit/commands/CommandCd.java
@@ -4,6 +4,7 @@ import com.beust.jcommander.Parameter;
 import ru.spbau.mit.Environment;
 
 import java.io.*;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -22,11 +23,26 @@ public class CommandCd implements Command {
     @Override
     public void run(InputStream is, OutputStream os, Environment environment) throws IOException {
         File newDir;
+        String newDirString;
         if (parameters.size() > 0) {
-            newDir = new File(parameters.get(0));
+            newDirString = parameters.get(0);
         } else {
             Scanner scanner = new Scanner(is);
-            newDir = new File(scanner.next());
+            newDirString = scanner.next();
+        }
+        if (newDirString.equals("..")) {
+            newDirString = Paths.get(environment.getCurDir()).getParent().toString();
+        } else if (newDirString.charAt(0) == '.') {
+            newDirString = environment.getCurDir() + newDirString.substring(1);
+        } else if (newDirString.charAt(0) != File.separatorChar) {
+            newDirString = Paths.get(environment.getCurDir(), newDirString).toString();
+        }
+        newDir = new File(newDirString);
+        if (!newDir.exists()) {
+            throw new IOException("The directory doesn't exist");
+        }
+        if (!newDir.isDirectory()) {
+            throw  new IOException("It is file, not directory");
         }
         environment.setCurDir(newDir.getAbsolutePath());
     }
