948b911c6e46c05597efcb7b1034644b2f6e7bf3
N-buga <bugakova_nadya@mail.ru>
2016-12-19 00:35:13 +0300

    Add rile dor temporary files

--- src/test/java/ru/spbau/mit/commands/CommandsLsCdTest.java
+++ src/test/java/ru/spbau/mit/commands/CommandsLsCdTest.java
@@ -1,7 +1,9 @@
 package ru.spbau.mit.commands;
 
 import com.beust.jcommander.JCommander;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import ru.spbau.mit.Environment;
 
 import java.io.*;
@@ -18,51 +20,46 @@ import static junit.framework.Assert.assertTrue;
  * Created by n_buga on 02.12.16.
  */
 public class CommandsLsCdTest {
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
     @Test
     public void testRun1() throws IOException {
         Path curPath = Paths.get(System.getProperty("user.dir"));
         Path tempDir = Files.createTempDirectory(curPath, "");
-        try {
-            Command commandLs = new CommandLs();
-            Command commandCd = new CommandCd();
-            Environment curEnv = new Environment();
-            PipedOutputStream currentOutputStream = new PipedOutputStream();
-            InputStream nextInputStream = new PipedInputStream(currentOutputStream);
-            new JCommander(commandCd, tempDir.toString());
-            commandCd.run(System.in, currentOutputStream, curEnv);
-            assertEquals(tempDir.toAbsolutePath().toString(), curEnv.getCurDir());
-            new JCommander(commandLs, "a bc");
-            commandLs.run(nextInputStream, currentOutputStream, curEnv);
-            currentOutputStream.close();
-            Scanner in = new Scanner(nextInputStream);
-            assertFalse(in.hasNextLine());
-        } finally {
-            Files.deleteIfExists(tempDir);
-        }
+        Command commandLs = new CommandLs();
+        Command commandCd = new CommandCd();
+        Environment curEnv = new Environment();
+        PipedOutputStream currentOutputStream = new PipedOutputStream();
+        InputStream nextInputStream = new PipedInputStream(currentOutputStream);
+        new JCommander(commandCd, tempDir.toString());
+        commandCd.run(System.in, currentOutputStream, curEnv);
+        assertEquals(tempDir.toAbsolutePath().toString(), curEnv.getCurDir());
+        new JCommander(commandLs, "a bc");
+        commandLs.run(nextInputStream, currentOutputStream, curEnv);
+        currentOutputStream.close();
+        Scanner in = new Scanner(nextInputStream);
+        assertFalse(in.hasNextLine());
     }
     @Test
     public void testRun2() throws IOException {
         Path curPath = Paths.get(System.getProperty("user.dir"));
         Path tempDir = Files.createTempDirectory(curPath, "");
         Path tempFile = Files.createTempFile(tempDir, "", "");
-        try {
-            Command commandLs = new CommandLs();
-            Command commandCd = new CommandCd();
-            Environment curEnv = new Environment();
-            PipedOutputStream currentOutputStream = new PipedOutputStream();
-            InputStream nextInputStream = new PipedInputStream(currentOutputStream);
-            new JCommander(commandCd, tempDir.toString());
-            commandCd.run(System.in, currentOutputStream, curEnv);
-            assertEquals(tempDir.toAbsolutePath().toString(), curEnv.getCurDir());
-            new JCommander(commandLs, "a bc");
-            commandLs.run(nextInputStream, currentOutputStream, curEnv);
-            currentOutputStream.close();
-            Scanner in = new Scanner(nextInputStream);
-            assertTrue(in.hasNextLine());
-            assertEquals(in.nextLine(), tempFile.getFileName().toString());
-        } finally {
-            Files.deleteIfExists(tempFile);
-            Files.deleteIfExists(tempDir);
-        }
+
+        Command commandLs = new CommandLs();
+        Command commandCd = new CommandCd();
+        Environment curEnv = new Environment();
+        PipedOutputStream currentOutputStream = new PipedOutputStream();
+        InputStream nextInputStream = new PipedInputStream(currentOutputStream);
+        new JCommander(commandCd, tempDir.toString());
+        commandCd.run(System.in, currentOutputStream, curEnv);
+        assertEquals(tempDir.toAbsolutePath().toString(), curEnv.getCurDir());
+        new JCommander(commandLs, "a bc");
+        commandLs.run(nextInputStream, currentOutputStream, curEnv);
+        currentOutputStream.close();
+        Scanner in = new Scanner(nextInputStream);
+        assertTrue(in.hasNextLine());
+        assertEquals(in.nextLine(), tempFile.getFileName().toString());
     }
 }
